7d65f2495c38c3395a8725c20ff50773ce595852
Vedarshi Shastry <75200682+vedshastry@users.noreply.github.com>
2025-03-06 10:29:12 -0500

    update nvim stata

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,9 +1,11 @@
 -- ~/.config/nvim/lua/plugins/init.lua
 return {
-  -- Import all other plugin files
+
+  -- Import plugin files
   require("plugins.colorscheme"),
   require("plugins.treesitter"),
   require("plugins.telescope"),
   require("plugins.quarto"),
-  -- Add more plugin imports as you create them
+  require("plugins.stata"),
+  
 }
