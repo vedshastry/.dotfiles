9b864e4f0d99c8ecbd400bb2be343acbf5108012
Vedarshi Shastry <75200682+vedshastry@users.noreply.github.com>
2025-03-05 22:38:02 -0500

    dwm

---
+++ dwm/.dwm/autostart.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+# function to launch a program if it is not already running
+function run {
+ if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+
+run	"nitrogen --restore"
+# Keymap
+run	"setxkbmap us"
+# Picom
+run	"picom" 
+run	"xsetroot -cursor_name left_ptr"
+# Wallpaper
+run	"nitrogen --restore"
+# Fonts
+run	"xset +fp ~/.fonts/misc"
+# Pulseaudio
+run	"pulseaudio --start"
+# Numlock
+run	"numlockx"
+# Dunst notify
+run	"dunst"
+# KeepassXC
+run	"keepassxc"
+# Syncthing
+run	"syncthing -no-browser -no-restart -logflags=3"
+# Network manager applet
+run	"nm-applet"
+# Power Manager
+run	"xfce4-power-manager"
+# Solaar
+#run	"solaar"
+# Bluetooth
+run	"blueman-applet"
+# Audio
+run	"pasystray"
+# Dropbox
+run	"dropbox"
+#run	"maestral"
+# Onedrive systray
+#run	"onedrive_tray"
+# Ferdi
+#run	"ferdi"
+# Slack
+run 	"slack -u"
+# Teams
+#run 	"teams"
+# Flameshot 
+run	"flameshot"
+# Redshift 
+#run	"redshift-gtk"
+# Thunderbird
+#run	"kdocker -l thunderbird"
+# Emacs daemon
+run "emacs --daemon"
+# Slstatus
+run	"slstatus"
+# touchegg
+run "touchegg --daemon"
+run "touchegg"
---
+++ dwm/.dwm/autostart_blocking.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+#$HOME/.config/polybar/launch.sh
